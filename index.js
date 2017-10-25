abbda2ae4c851958e8e88bfeef7ed4b0099a0c82
rf-info <rf.info0525@gmail.com>
2017-10-25 01:16:17 -0500

    Kick test

--- index.js
+++ index.js
@@ -6,21 +6,27 @@ const client = new Discord.Client();
 
 const prefix = ".";
 
+const args = message.content.slice(prefix.length).trim().split(/ +/g);
+
+const command = args.shift().toLowerCase();
+
 client.on('ready', () => {
   console.log('Tips is operational.');
 });
 
 // Ping pong test.
-client.on('message', (message) => {
-  if (!message.content.startsWith(prefix) || message.author.bot) return;
+if (command === 'ping') {
+  message.channel.send('Pong!');
+} else
+if (command === 'Cadence') {
+  message.channel.send('Hook up, shuffle up to the door! Jump right out and count to four.');
+}
 
-  if (message.content.startsWith(prefix + "ping")) {
-    message.reply('Pong!');
-  } else
-  if (message.content.startsWith(prefix + "Hook up shuffle up to the door!")) {
-    message.channel.send("Jump right out and count to four!");
-  }
-});
+// Kicking someone.
+if (command === 'kick') {
+  let member = message.mentions.members.first();
+  member.kick();
+}
 
 // Retrieves the token from .config
 client.login(config.token);
