eabde03861aadddf4fa9ceddc4f28e225c627439
ScottyYDG <33048440+ScottyYDG@users.noreply.github.com>
2017-10-25 00:01:32 -0700

    Kick Test pt 2

--- commands/kick.js
+++ commands/kick.js
@@ -1,21 +1,17 @@
 exports.run = (client, message, [mention, ...reason]) => {
-    const modRole = message.guild.roles.find("name", "Mods");
-    if (!modRole)
-      return console.log("The Mods role does not exist");
-  
-    if (!message.member.roles.has(modRole.id))
-      return message.reply("You can't use this command.");
-  
-    if (message.mentions.users.size === 0) {
-      return message.reply("Please mention a user to kick");
-  
-      if (!message.guild.me.hasPermission("KICK_MEMBERS"))
-        return message.reply("");
-  
-      const kickMember = message.mentions.members.first();
-  
-      kickMember.kick(reason.join(" ")).then(member => {
-        message.reply(`${member.user.username} was succesfully kicked.`);
-      });
-    }
-  };
+  if(command === "kick") 
+  {
+    if(!message.member.roles.some(r=>["Administrator", "Moderator"].includes(r.name)) )
+      return message.reply("Sorry, you don't have permissions to use this!");
+    let member = message.mentions.members.first();
+    if(!member)
+      return message.reply("Please mention a valid member of this server");
+    if(!member.kickable) 
+      return message.reply("I cannot kick this user! Do they have a higher role? Do I have kick permissions?");
+    let reason = args.slice(1).join(' ');
+    if(!reason)
+      return message.reply("Please indicate a reason for the kick!");
+    await member.kick(reason)
+      .catch(error => message.reply(`Sorry ${message.author} I couldn't kick because of : ${error}`));
+    message.reply(`${member.user.tag} has been kicked by ${message.author.tag} because: ${reason}`);
+  }
