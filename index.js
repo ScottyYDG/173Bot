c743e2e67724f30240ac669d0ec8bef338185a53
rf-info <rf.info0525@gmail.com>
2017-10-25 00:54:48 -0500

    prefix additon, message event edits.

--- index.js
+++ index.js
@@ -4,14 +4,21 @@ const config = require('./config.json');
 
 const client = new Discord.Client();
 
+const prefix = ".";
+
 client.on('ready', () => {
   console.log('Tips is operational.');
 });
 
 // Ping pong test.
 client.on('message', (message) => {
-  if (message.content === 'ping') {
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+  if (message.content.startsWith(prefix + "ping")) {
     message.reply('Pong!');
+  } else
+  if (message.content.startsWith(prefix + "Hook up shuffle up to the door!")) {
+    message.channel.send("Jump right out and count to four!");
   }
 });
 
