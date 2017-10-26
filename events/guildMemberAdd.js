7cccfc2f13ce3438e89d9f6ef3754510dc616d0a
ScottyYDG <33048440+ScottyYDG@users.noreply.github.com>
2017-10-26 00:32:57 -0700

    fuck me

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,6 @@ module.exports = (client, member) => {
   client.log('Log', `${member.user.tag} (${member.id}) has joined ${member.guild.name} (${member.guild.id})`);
   const welcomeChannel = member.guild.channels.find('name', 'the-rock-pit' || 'barracks');
   if (welcomeChannel) {
-    welcomeChannel.send(`${member.user} Congratulations, privates. Welcome to the 173rd Airborne Brigade. You are now a Sky Soldier`);
+    welcomeChannel.send(`${member.user} Contact an avalible DS to get squared away.`);
   }
 };
